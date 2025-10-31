f512c8dc6c8373ef4d2b12ef49d43a0a8552f6e9
Sravya Konda <srko2106@hfest.vfikt.no>
2025-10-31 13:28:02 +0100

    updated

--- helse-frontend/vite.config.js
+++ helse-frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 const repoName = "Helse-Omsorg"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: `/${repoName}/`,
-  build: { outDir: "docs" }
- 
+  build: {
+    outDir: "docs"   
+  }
 })
